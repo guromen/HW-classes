04d16e0f93b272300a0c8596e13640301f3cb591
Roman Guguzin <r-0men@yandex.ru>
2024-06-02 11:38:37 +0400

    Задание 4

--- Home_work.py
+++ Home_work.py
@@ -6,6 +6,23 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+
+    def mid_grade(self):
+        if not self.grades:
+            return 0
+        list_grade=[] 
+        for v in self.grades.values():
+            list_grade.extend(v)
+        return round(float(sum([n for n in list_grade])/len(list_grade)), 1)
+
+    def __str__(self):
+        res = f"Имя: {self.name}\n" \
+              f"Фамилия: {self.surname}\n" \
+              f"Средняя оценка за домашнее задание: {self.mid_grade()}\n" \
+              f"Курсы в процессе обучения: {', '.join(self.courses_in_progress)}\n" \
+              f"Завершенные курсы: {', '.join(self.finished_courses)}"
+        return res
+
     def rate_hw(self, lecturer, course, grade):
         if isinstance(lecturer,Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
            if course in lecturer.grades:
@@ -14,14 +31,7 @@ class Student:
                lecturer.grades[course] = [grade]
         else:
             return'Ошибка'
-    
-    def __str__(self):
-        res = f"Имя: {self.name}\n" \
-              f"Фамилия: {self.surname}\n" \
-              f"Средняя оценка за домашнее задание: {self.mid_grade()}\n" \
-              f"Курсы в процессе обучения: {', '.join(self.courses_in_progress)}\n" \
-              f"Завершенные курсы: {', '.join(self.finished_courses)}"
-        return res
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -34,6 +44,15 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+
+
+    def mid_grade(self):
+        if not self.grades:
+            return 0
+        list_grade=[]
+        for v in self.grades.values():
+           list_grade.extend(v)
+        return sum([n for n in list_grade])/len(list_grade)
 
     def __str__(self):
         return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.mid_grade()}'
@@ -57,6 +76,63 @@ class Reviewer(Mentor):
     def __str__(self):
         print(f'Имя: {self.name}\nФамилия: {self.surname}\n')
 
+lecturer_1 = Lecturer('Иван', 'Иванов')
+lecturer_1.courses_attached += ['Python']
+
+lecturer_2 = Lecturer('Petr', 'Petrov')
+lecturer_2.courses_attached += ['Java']
+
+rewiewer_1 = Reviewer('Some', 'Buddy')
+rewiewer_1.courses_attached += ['Python']
+rewiewer_1.courses_attached += ['Java']
+
+rewiewer_1.__str__()
+
+rewiewer_2 = Reviewer('Марь', 'Иванна')
+rewiewer_2.courses_attached += ['Python']
+rewiewer_2.courses_attached += ['Java']
+
+student_1 = Student('Roman', 'Guguzin')
+student_1.courses_in_progress += ['Python']
+student_1.finished_courses += ['Введение в программирование']
+student_1.finished_courses += ['Git']
+
+student_2 = Student('Mikhail', 'Krug')
+student_2.courses_in_progress += ['Java']
+student_2.finished_courses += ['Введение в программирование']
+
+#создал 3й экземпляр класса Student, чтобы отработать пример с рейтингом 
+student_3 = Student('Arkadiy', 'Ukupnik')
+student_3.courses_in_progress += ['Python']
+student_3.finished_courses += ['Введение в программирование']
+
+student_1.rate_hw(lecturer_1, 'Python', 10)
+student_1.rate_hw(lecturer_1, 'Python', 10)
+student_1.rate_hw(lecturer_1, 'Python', 10)
+
+student_2.rate_hw(lecturer_2, 'Java', 10)
+student_2.rate_hw(lecturer_2, 'Java', 5)
+student_2.rate_hw(lecturer_2, 'Java', 6)
+
+rewiewer_1.rate_hw(student_1, 'Python', 10)
+rewiewer_1.rate_hw(student_1, 'Python', 9)
+rewiewer_1.rate_hw(student_1, 'Python', 10)
+
+rewiewer_2.rate_hw(student_2, 'Java', 8)
+rewiewer_2.rate_hw(student_2, 'Java', 7)
+rewiewer_2.rate_hw(student_2, 'Java', 9)
+
+rewiewer_2.rate_hw(student_3, 'Python', 7)
+rewiewer_2.rate_hw(student_3, 'Python', 6)
+rewiewer_2.rate_hw(student_3, 'Python', 8)
+
+print(f'Студенты:\n\n{student_1}\n\n{student_2}\n\n{student_3}')
+print()
+
+
+print(f'Лекторы:\n\n{lecturer_1}\n\n{lecturer_2}')
+print()
+
 #min, max студентов
 m = max(student_1.mid_grade(),student_2.mid_grade())
 n = min(student_1.mid_grade(),student_2.mid_grade())
@@ -72,4 +148,27 @@ b = min(lecturer_1.mid_grade(),lecturer_2.mid_grade())
 
 print(f'Результат сравнения лекторов (по средним оценкам за лекции):\n ' \
       f'{lecturer_1.name} {lecturer_1.surname} > {lecturer_2.name} {lecturer_2.surname} = {lecturer_1.mid_grade() > lecturer_2.mid_grade()} ({a} > {b})')
+print()
+
+student_list = [student_1 , student_2, student_3] 
+lecturer_list = [lecturer_1, lecturer_2]
+
+def student_rating(student_list, course_name):
+    count_all = []
+    for stud in student_list:
+        if stud.courses_in_progress == [course_name]:
+            count_all.append(stud)
+    return round(float(sum(n.mid_grade() for n in count_all)/len(count_all)), 1)
+
+def lecturer_rating(lecturer_list, course_name):
+    count_all = []
+    for lect in lecturer_list:
+        if lect.courses_attached == [course_name]:
+            count_all.append(lect)
+    return round(float(sum(n.mid_grade() for n in count_all)/(len(count_all))), 1)
+
+print(f"Средняя оценка для всех студентов по курсу {'Python'}: {student_rating(student_list, 'Python')}")
+print()
+
+print(f"Средняя оценка для всех лекторов по курсу {'Python'}: {lecturer_rating(lecturer_list, 'Python')}")
 print()
